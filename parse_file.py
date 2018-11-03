2147b7902e2904d1519a3b5a429dc30c00538974
Ray Yuan <ray.x.yuan@gmail.com>
2018-11-03 05:24:37 -0700

    fixed little bug with no call

--- parse_file.py
+++ parse_file.py
@@ -34,38 +34,49 @@ def create_graph(file_list,src_code_dict):
 		#after we get returned a list from above function call, we add to the set below
 		for elem in file_functions[curr_file]:
 			all_functions[elem]=1
-	#print(file_list)
+
 	# creates CallData objects for all funcs
 	call_data_objects=[]
 	for src_key in file_functions:
 		src_file=src_code_dict[src_key]
-		#print(src_file)
 		curr_src_split=src_file.split("\n")#essentially makes raw file into lines of a txt file
 		curr_funcs=file_functions[src_key]
-		#print(curr_src_split)
+
 		for func in curr_funcs:
-			curr_search="def "+func+":" #just in case u call this function above the def
+			curr_search="def "+func #just in case u call this function above the def
 			curr_call_list=[]
 
 			#get to next def
-			while curr_search not in curr_src_split[0]:
+			while curr_src_split!=[] and curr_src_split !=[''] and curr_search not in curr_src_split[0]:
 				curr_src_split=curr_src_split[1:]
 			curr_src_split=curr_src_split[1:]
 
-			while curr_src_split !=[] and curr_src_split != [''] and curr_src_split[0][0]==" ": #check if space is first char to make sure actually under def
+			while curr_src_split[0]!='' and curr_src_split !=[] and curr_src_split != [''] and curr_src_split[0][0]==" ": #check if space is first char to make sure actually under def
 				for funcs in all_functions:
-					if funcs in curr_src_split[0]:#can always make it like curr_search later
+					funcs1=funcs+"()"
+					if funcs1 in curr_src_split[0]:#can always make it like curr_search later
 						curr_call_list.append(funcs)#potentially could call same func multiple times. Maybe in future implement counter, and make arrow thi
 				curr_src_split=curr_src_split[1:]
 
 			call_data_objects.append(CallData(func,src_file,curr_call_list))
 
-	return call_data_objects[0].get_call_list()
+	#return call_data_objects[0].get_call_list()
+	return call_data_objects
+
 #get passed in iterable of all lines in file
 def get_functions(file_string):
-	func_list = re.findall(r'def \w*:', file_string)
-	return [func_line[func_line.find(" ")+1:func_line.find(":")] for func_line in func_list]
+	#func_list = re.findall(r'def \w*:', file_string)
+	#return [func_line[func_line.find(" ")+1:func_line.find(":")] for func_line in func_list]
+	ret=[]
+	curr_src_split=file_string.split("\n")
+	for line in curr_src_split:
+		if line[0:3] == "def":
+			ret.append(line[line.find(" ")+1:line.find("(")])
+	return ret
 
-testDict={"src1":"def a:\n  b() \n  c()\nc()\ndef b:\ndef c:"}
+testDict={"src1":'\nimport sys\n\nSTAT_START_COL = {"letter": \'F\', "number": 6}\n\ndef get_time():\n    TIME_FORMAT = "%Y-%m-%d"\n    get_time()\n    return strftime(TIME_FORMAT, gmtime())\n\ndef initialize_metadata():\n    METADATA["comics sent"] = METADATA["comics sent"].format(*letters)\n    STAT_START_COL\n    METADATA["MRCN"] = scrape_utils.most_recent_comic_num()\n\ndef run_setup():\n    # CREATE SHEETS].value = md_items[i][1]\n    reset()\n    initialize_metadata()\n\ndef reset():\n    wb = db_client.wb\n    get_time\n\n# NOTE: Doesn\'t run if not specifically running setup - file should only be run once.\nif __name__ == "__main__":\n    print("dummy")\n    reset()\n'}
 testStr=["src1"]
-print(create_graph(testStr,testDict))
+fml = create_graph(testStr,testDict)
+for elem in fml:
+	#print(elem.get_func_name()+" --> "+elem.get_call_list())
+	print (elem.get_func_name(),'-->',elem.get_call_list())
